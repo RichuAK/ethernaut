845d6faf8ab6e27a0947764413d4719c61a4101b
richuak <richuakuttikattu@gmail.com>
2024-01-15 20:59:49 +0300

    some more edits

--- GatekeeperOne/notes.md
+++ GatekeeperOne/notes.md
@@ -42,6 +42,9 @@ gateKey: 0x005e9ba00000fcdc
 Anvil Account(0) : 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 gateKey: 0x827279cf00002266
 
+Some random address from forge test: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
+gateKey: 0x83f33e0700001f38
+
 `cast` method to send eth to a different address on a local chain, and then use that address using `vm.prank()` for testing:
 
 cast send --value 1000000000000000000000 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 0xcF78399B272E71F23F00b453005e9ba0EFa9FcDc
--- prototyping/test/TestGateKeeperOne.t.sol
+++ prototyping/test/TestGateKeeperOne.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.0;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {GatekeeperOne} from "../src/Gatekeeperone.sol";
 import {EnterGateKeeperOne} from "../src/EnterGatekeeperone.sol";
 import {DeployGateKeeperOne} from "../script/DeployGateKeeperOne.s.sol";
@@ -11,11 +11,32 @@ import {DeployEnterGateKeeperOne} from "../script/DeployEnterGateKeeperOne.s.sol
 contract TestGateKeeperOne is Test {
     GatekeeperOne gateKeeperOne;
     EnterGateKeeperOne enterGateKeeperOne;
+    // bytes8 gateKey = bytes8(0x827279cf00002266);
+    uint256 gas = 10000000;
 
     function setUp() external {
         DeployGateKeeperOne deployGateKeeperOne = new DeployGateKeeperOne();
         gateKeeperOne = deployGateKeeperOne.run();
         DeployEnterGateKeeperOne deployEnterGateKeeperOne = new DeployEnterGateKeeperOne(address(gateKeeperOne));
         enterGateKeeperOne = deployEnterGateKeeperOne.run();
+    }
+
+    function testGatePassesWithoutGateTwo() public {
+        vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        console.log("Sender address: ", tx.origin);
+        console.log("Msg Sender: ", msg.sender);
+        console.log("Address Balance:", address(tx.origin).balance);
+        enterGateKeeperOne.youShallPass(gas, 0x83f33e0700001f38);
+        assert(gateKeeperOne.entrant() == 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38);
+    }
+
+    function testAllGatesPassed(uint256 gassy) public {
+        vm.assume(gassy > 819000);
+        vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        // console.log("Sender address: ", tx.origin);
+        // console.log("Msg Sender: ", msg.sender);
+        // console.log("Address Balance:", address(tx.origin).balance);
+        enterGateKeeperOne.youShallPass(gassy, 0x83f33e0700001f38);
+        assert(gateKeeperOne.entrant() == 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38);
     }
 }

